eebe3105bbd1ef116e8b6ff9f2ab23ef1a40b101
Hai Nguyen <hainguyen@ip-10-1-9-98.ec2.internal>
2020-02-27 15:08:24 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, createRef} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const MyInputs = React.forwardRef((props, ref) => {
+    console.log(ref.current)
+    return (
+        <>
+            <input name="input1" ref={ref}/>
+            <input name="input2"/>
+        </>
+    )
+})
+
+const App = () => {
+    const [email, setEmail] = useState("")
+    const [address, setAddress] = useState("")
+    let emailInputRef = null
+    let addressInputRef = null
+    const input1Ref = createRef()
+
+    const setEmailInputRef = element => {
+        emailInputRef = element
+    }
+
+    const setAddressInputRef = element => {
+        addressInputRef = element
+    }
+
+    const focusEmailInput = () => {
+        if (emailInputRef) emailInputRef.focus()
+    }
+
+    const focusAddressInput = () => {
+        if (addressInputRef) addressInputRef.focus()
+    }
+
+    const incrementEmail = () => {
+        emailInputRef.value++
+    }
+
+    const focusInput1 = () => {
+        if (input1Ref) input1Ref.current.focus()
+    }
+
+    console.log(email)
+
+    return (
+        <div className="App">
+            <input
+                name="email"
+                onChange={e => setEmail(e.target.value)}
+                ref={setEmailInputRef}
+                type="text"
+                value={email}
+            />
+            <input
+                name="address"
+                onChange={e => setAddress(e.target.value)}
+                ref={setAddressInputRef}
+                type="text"
+                value={address}
+            />
+            <button onClick={() => focusEmailInput()}>Focus Email Input</button>
+            <button onClick={() => incrementEmail()}>Increment Email</button>
+            <button onClick={() => focusAddressInput()}>Focus Address Input</button>
+            <MyInputs ref={input1Ref}/>
+            <button onClick={() => focusInput1()}>Focus Input 1</button>
+        </div>
+    )
 }
 
-export default App;
+export default App
+
+// const [email, setEmail] = useState("")
+// const emailInputRef = createRef()
+//
+// const onEmailChange = e => setEmail(e.target.value)
+//
+// console.log(emailInputRef)
+// console.log(emailInputRef.current)
+//
+// return (
+//     <div className="App">
+//         <input
+//             name="email"
+//             onChange={onEmailChange}
+//             ref={emailInputRef}
+//             type="text"
+//             value={email}
+//         />
+//         <button onClick={() => emailInputRef.current.focus()}>Focus Email Input</button>
+//     </div>
+// );
